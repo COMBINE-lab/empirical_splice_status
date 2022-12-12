b3b7c139e44c6992024513d2dba3daeeff4470f7
Rob Patro <rob@newton>
2022-12-12 00:34:52 -0500

    initial commit

---
+++ src/bin/build_index.rs
@@ -0,0 +1,47 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::{Path, PathBuf};
+use suffine::MultiDocIndexBuilder;
+use std::env::args; 
+use kseq::parse_path;
+use bincode::serialize_into;
+
+
+use serde::{Serialize, Deserialize};
+use bincode;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TextInfo {
+  pub text: String,
+  pub target_names: Vec<String>
+}
+
+fn main() {
+    let m_index_base = args().nth(2).unwrap();
+    let m_text_file = File::create(m_index_base.clone() + ".btex").unwrap();
+    let m_index_file = File::create(m_index_base + ".idx").unwrap();
+    let mut m_index_writer = BufWriter::new(m_index_file);
+
+    let path: String = args().nth(1).unwrap();
+	let mut records = parse_path(path).unwrap();
+    let mut txp_info = TextInfo{text : String::new(), target_names : vec![]};
+    //let mut txp_strings = String::new();
+	// let mut records = parse_reader(File::open(path).unwrap()).unwrap();
+	while let Some(record) = records.iter_record().unwrap() {
+        txp_info.text += record.seq();
+        txp_info.text.push('#');
+        txp_info.target_names.push(record.head().to_string());
+	}
+    
+    {
+        let mut m_text_writer = BufWriter::new(m_text_file);
+        serialize_into(&mut m_text_writer, &txp_info).unwrap();
+        m_text_writer.flush().unwrap();
+    }
+
+    let _multi_doc_index = MultiDocIndexBuilder::new(&txp_info.text)
+    .delimiter('#')
+    .build_to_writer_native_endian(&mut m_index_writer).unwrap();
+
+    m_index_writer.flush().unwrap();
+}
---
+++ src/bin/map_queries.rs
@@ -0,0 +1,130 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::{Path, PathBuf};
+use suffine::MultiDocIndex;
+use std::env::args; 
+use kseq::parse_path;
+use memmap::Mmap;
+use anyhow::Result;
+use std::fmt;
+
+fn open_and_map<P: AsRef<Path>>(path: P) -> Result<Mmap> {
+    let file = File::open(&path)?;
+    let mmap = unsafe { Mmap::map(&file)? };
+    Ok(mmap)
+}
+
+use bincode::serialize_into;
+
+
+use serde::{Serialize, Deserialize};
+use bincode;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TextInfo {
+  pub text: String,
+  pub target_names: Vec<String>
+}
+
+enum SplicingStatus {
+    MATURE,
+    NASCENT,
+    AMBIGUOUS
+}
+
+impl fmt::Display for SplicingStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match *self {
+           SplicingStatus::MATURE => write!(f, "m"),
+           SplicingStatus::NASCENT => write!(f, "n"),
+           SplicingStatus::AMBIGUOUS => write!(f, "a"),
+       }
+    }
+}
+
+
+fn revcomp(dna: &str) -> String{
+    // result vector
+    let mut rdna: String = String::with_capacity(dna.len()); 
+
+    // iterate through the input &str
+    for c in dna.chars().rev() {
+        // test the input
+        match is_dna(c) {
+            false => panic!("Input sequence base is not DNA: {}", dna),
+            true => rdna.push(switch_base(c))
+        }
+    }
+    rdna
+}
+
+fn switch_base(c:char) -> char {
+    match c {
+        'a' => 't' ,
+        'c' => 'g' ,
+        't' => 'a' ,
+        'g' => 'c' ,
+        'u' => 'a',
+        'A' => 'T' ,
+        'C' => 'G' ,
+        'T' => 'A' ,
+        'G' => 'C',
+        'U' => 'A',
+        _ => 'N'
+    }
+}
+
+fn is_dna(dna: char) -> bool {
+    match dna {
+        'A' | 'a' | 'C' | 'c' | 'G' | 'g' | 'T' | 't' | 'U'| 'u'  => true,
+        _ => false
+    }
+}
+
+
+
+fn main() -> Result<(), anyhow::Error> {
+    let base_filename = args().nth(1).unwrap();
+    let text_filename = base_filename.clone() + ".btex";
+    let idx_filename = base_filename.clone() + ".idx";
+
+    let text_file = File::open(text_filename).unwrap();
+    let mut reader = BufReader::new(text_file);
+    let mut txp_info: TextInfo = bincode::deserialize_from(&mut reader)?;
+
+    let m_index_mmap = open_and_map(idx_filename)?;
+    let multi_doc_index = MultiDocIndex::from_bytes(&txp_info.text, &m_index_mmap)?;
+
+    let path: String = args().nth(2).unwrap();
+	let mut records = parse_path(path).unwrap();
+    let mut txp_strings = String::new();
+	// let mut records = parse_reader(File::open(path).unwrap()).unwrap();
+	while let Some(record) = records.iter_record().unwrap() {
+        let query = record.seq();
+        let query_rc = revcomp(query);
+        let mut had_nascent = false;
+        let mut had_mature = false;
+        let mut status : Option<SplicingStatus> = None;
+        for q in [query, &query_rc] {
+        for (doc_id, pos) in multi_doc_index.doc_positions(q) {
+            let doc_name = &txp_info.target_names[doc_id as usize];
+            if doc_name.ends_with("-I") {
+                had_nascent = true;
+                status = Some(SplicingStatus::NASCENT);
+            } else {
+                had_mature = true;
+                status = Some(SplicingStatus::MATURE);
+            }
+            if had_nascent && had_mature {
+                status = Some(SplicingStatus::MATURE);
+                break;
+            }
+        }
+        if let Some(s) = status {
+            println!("{}\t{}", record.head(), s);
+            break;
+        } 
+        }
+	}
+    Ok(())
+}
